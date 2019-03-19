9bb21b813f7f2e54307697fedc0e0fb1686dc499
Billy <billy@globaltllc.com>
2019-03-20 00:41:30 +1100

    email verify

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Notifiable;
 

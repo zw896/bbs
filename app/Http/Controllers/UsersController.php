dad4ad5d73ef5f7d1e01543a9a79c4b0706fef5a
Billy <billy@globaltllc.com>
2019-03-20 16:31:49 +1100

    UserPolicy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -16,11 +21,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
@@ -31,6 +38,6 @@ class UsersController extends Controller
         }
 
         $user->update($data);
-        return redirect()->route('users.show', $user->id)->with('success', 'Your info has been updated.');
+        return redirect()->route('users.show', $user->id)->with('success', 'Your info has been updated!');
     }
 }

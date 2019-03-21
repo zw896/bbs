10dadd5279faf4861f63d2719d2394147c6cba3e
Billy <billy@globaltllc.com>
2019-03-21 14:13:12 +1100

    sort topics

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // Read the topic associated with the category ID and page through every 20
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->paginate(20);
+
         // Pass the variable topic and classify into the template
         return view('topics.index', compact('topics', 'category'));
     }
